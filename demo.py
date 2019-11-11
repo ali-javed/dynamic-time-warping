6183232e72bdf09209f0f53202e66477958f5ee8
ali-javed <32461180+ali-javed@users.noreply.github.com>
2019-11-11 15:41:45 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,3 +1,13 @@
+#If you use the code in your work please cite as
+#@misc{DTW_D,
+#title={Multivariate time series dynamic time warping using euclidean distance},
+#author={Ali Javed},
+#year={2019},
+#month={November},
+#note = {\url{https://github.com/ali-javed/dynamic-time-warping}}
+#}
+
+
 from DTW_D import dtw_d
 import numpy as np
 
